f84ebee978f11a1887fd39bfea1570e84a3c2d01
Muhammad Alinoor <alinoor145387@outlook.com>
2021-08-02 10:47:22 +0600

    basic-variable-recape

---
+++ recap-var.js
@@ -0,0 +1,28 @@
+// five things must be used in variable
+var identifiersName = "numeric, string or boolean value";
+// numeric variable stores number
+var numberValueAcceptor = 35;
+var banglaMarks = 75; // in percentage
+var waterQuantity = 980; // in mili litre
+var watchedVedio = 40;
+var subscribedYoutube = 2000000000000;
+var newVisitorsToday = 50000;
+var lastMonthSale = 10000000;
+var buyingPaddy = 2.5; // in kg
+// string variable stores human using English 
+var stringVariable = "Write in double or single quotation";
+var friendsName = "Sohanur Rahman";
+var myFirstName = "Ali";
+var myLastName = 'Noor';
+var touristSpot = 'Rangamati';
+var implementingMonth = "July";
+// boolean variable stores two values are true or false. Use to check yes or no question.
+var isRaining = true;
+var isLoving = false;
+var isGoing = true;
+var isBad = true;
+var areCriminal = false;
+// use console.log() to see the output of any operation
+console.log(touristSpot);//string output will give
+console.log(isLoving);//boolean output will give
+console.log(banglaMarks); // numeric output will shown
